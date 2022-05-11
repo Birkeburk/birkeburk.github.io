f04591dd75a041f9ea0a93a6411a27dbde84a9c7
Birkeburk <birkeburk@LAPTOP-DG0QU81H.localdomain>
2022-05-12 00:04:13 +0200

    Uppdaterade Uppgift 2

--- Uppgift_2/app.js
+++ Uppgift_2/app.js
@@ -4,17 +4,8 @@ let addBtn = document.querySelector('#add')
 let subtractBtn = document.querySelector('#subtract')
 let equalsBtn = document.querySelector('#equals')
 let multiplyBtn = document.querySelector('#multiply')
-let oneBtn = document.querySelector('#one')
-let twoBtn = document.querySelector('#two')
-let threeBtn = document.querySelector('#three')
-let fourBtn = document.querySelector('#four')
-let fiveBtn = document.querySelector('#five')
-let sixBtn = document.querySelector('#six')
-let sevenBtn = document.querySelector('#seven')
-let eightBtn = document.querySelector('#eight')
-let nineBtn = document.querySelector('#nine')
-let zeroBtn = document.querySelector('#zero')
 let resetBtn = document.querySelector('#reset')
+let numbersBtn = document.querySelectorAll('#numbers')
 
 let variable1 = 0;
 let variable2 = '';
@@ -41,72 +32,49 @@ subtractBtn.addEventListener('click' , event => {
 });
 multiplyBtn.addEventListener('click' , event => {
     lastInputOperand = checkLastInput(display.textContent);
+    if(!checkDisplayEmpty(display.textContent)) {
     if(lastInputOperand){
-
-    } else if(!lastInputOperand) {
+        } else if(!lastInputOperand) {
         display.textContent += multiplyBtn.innerText;
+        }
     }
 });
 equalsBtn.addEventListener('click' , event => {
-    let sum = calculateDisplayContent(display.textContent);
+    if(!checkDisplayEmpty(display.textContent)){
+        let sum = calculateDisplayContent(display.textContent);
 
-    let ulList = document.querySelector('.resultsList');
+        let ulList = document.querySelector('.resultsList');
 
-    let listItem = document.createElement('li');
-    let importBtn = document.createElement('button');
-    let removeBtn = document.createElement('button');
+        let listItem = document.createElement('li');
+        let importBtn = document.createElement('button');
+        let removeBtn = document.createElement('button');
 
-    listItem.textContent = display.textContent + "=" + sum;
-    importBtn.textContent = 'import';
-    importBtn.className = 'resultButtons';
-    removeBtn.textContent = 'x';
-    removeBtn.className = 'resultButtons';
+        listItem.textContent = display.textContent + "=" + sum;
+        importBtn.textContent = 'import';
+        importBtn.className = 'resultButtons';
+        removeBtn.textContent = 'x';
+        removeBtn.className = 'resultButtons';
 
-    ulList.append(listItem);
-    ulList.append(importBtn);
-    ulList.append(removeBtn);
+        ulList.append(listItem);
+        listItem.append(importBtn, removeBtn);
 
-    removeBtn.addEventListener('click', event =>{
-        listItem.remove();
-        importBtn.remove();
-        removeBtn.remove();
+        removeBtn.addEventListener('click', event =>{
+            listItem.remove();
+        });
+        importBtn.addEventListener('click', event =>{
+            display.textContent = listItem.textContent.split("=")[0];
+        });
+
+        display.textContent = clearDisplay(display.textContent);
+        ulList.scrollTop = ulList.scrollHeight
+    }
+});
+for(let i = 0; i < numbersBtn.length; i++) {
+    numbersBtn[i].addEventListener('click', event =>{
+    display.textContent += numbersBtn[i].innerText;
     });
-    importBtn.addEventListener('click', event =>{
-        display.textContent = listItem.textContent.split("=")[0];
-    });
+}
 
-    display.textContent = clearDisplay(display.textContent);
-});
-oneBtn.addEventListener('click' , event => {
-    display.textContent += oneBtn.innerText;
-});
-twoBtn.addEventListener('click' , event => {
-    display.textContent += twoBtn.innerText;
-});
-threeBtn.addEventListener('click' , event => {
-    display.textContent += threeBtn.innerText;
-});
-fourBtn.addEventListener('click' , event => {
-    display.textContent += fourBtn.innerText;
-});
-fiveBtn.addEventListener('click' , event => {
-    display.textContent += fiveBtn.innerText;
-});
-sixBtn.addEventListener('click' , event => {
-    display.textContent += sixBtn.innerText;
-});
-sevenBtn.addEventListener('click' , event => {
-    display.textContent += sevenBtn.innerText;
-});
-eightBtn.addEventListener('click' , event => {
-    display.textContent += eightBtn.innerText;
-});
-nineBtn.addEventListener('click' , event => {
-    display.textContent += nineBtn.innerText;
-});
-zeroBtn.addEventListener('click' , event => {
-    display.textContent += zeroBtn.innerText;
-});
 resetBtn.addEventListener('click', event =>{
     display.textContent = clearDisplay(display.textContent);
 });
@@ -120,11 +88,18 @@ function clearDisplay(displayContent) {
 function checkLastInput(displayContent) {
  let lastInput = displayContent.slice(-1);
 
- if(lastInput === '+' || lastInput === '-' || lastInput === '*' || lastInput === '') {
+ if(lastInput === '+' || lastInput === '-' || lastInput === '*') {
      return true;
  } else{
      return false;
  }
+}
+function checkDisplayEmpty(displayContent) {
+    if(displayContent.length < 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 function calculateDisplayContent(displayContent){
     let sum = eval(displayContent)
